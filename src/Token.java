6430da9bfeafc6c0ea1157601fff9b14fb0f4f09
Philipp Müller <144794065+PhilippMueller0815@users.noreply.github.com>
2024-06-20 05:49:02 +0200

    Revert "Abgabe Übungsblatt 2 Helene Christ"

--- src/Token.java
+++ src/Token.java
@@ -7,21 +7,11 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.nio.charset.StandardCharsets;
 import java.util.LinkedList;
-import java.util.ListIterator;
 import java.util.Queue;
 
 public class Token {
 
     private static final int max_buffer_size = 4096;
-
-    private boolean isConfirmation;
-
-    public void setIsConfirmation(boolean isMessage) {
-        this.isConfirmation = isMessage;
-    }
-    public boolean getIsConfirmation() {
-        return isConfirmation;
-    }
 
     public record Endpoint(String ip, int port) {}
 
@@ -35,9 +25,6 @@ public class Token {
         return this;
     }
 
-    public void remove(Endpoint endpoint) {
-        ring.remove(endpoint);
-    }
     public Endpoint first() {
         return ring.peek();
     }
@@ -68,9 +55,8 @@ public class Token {
         String rc_json = toJSON();
         byte[] rc_json_bytes = rc_json.getBytes(StandardCharsets.UTF_8);
         InetAddress address = InetAddress.getByName(ip_address);
-        //System.out.println(address.isReachable(1000));
         DatagramPacket packet = new DatagramPacket(rc_json_bytes, rc_json_bytes.length, address, port);
-        //System.out.printf("Sending %s to %s:%d\n", rc_json, ip_address, port);
+        // System.out.printf("Sending %s to %s:%d\n", rc_json, ip_address, port);
         s.send(packet);
     }
 
@@ -79,7 +65,6 @@ public class Token {
     }
 
     public static Token receive(DatagramSocket s) throws IOException {
-
         byte[] buf = new byte[max_buffer_size];
         DatagramPacket packet = new DatagramPacket(buf, buf.length);
         s.receive(packet);
@@ -88,19 +73,8 @@ public class Token {
         return fromJSON(rc_json);
     }
 
-
-
     @JsonProperty
-    private final LinkedList<Endpoint> ring = new LinkedList<>();
-    private ListIterator<Endpoint> iterator = ring.listIterator();
-
-    public Endpoint previous() {
-        if (iterator.hasPrevious()) {
-            return iterator.previous();
-        } else {
-            return null;
-        }
-    }
+    private final Queue<Endpoint> ring = new LinkedList<>();
 
     public Queue<Endpoint> getRing() {
         return ring;
--- src/TokenRing.java
+++ src/TokenRing.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.net.*;
-import java.util.Arrays;
 import java.util.LinkedList;
 
 
@@ -10,62 +9,37 @@ public class TokenRing {
         LinkedList<Token.Endpoint> candidates = new LinkedList<>();
         if (first) {
             candidates.add(new Token.Endpoint(ip, port));
-            //System.out.println("Token erstellt");
         }
         while (true) {
             try {
-                socket.setSoTimeout(0);
                 Token rc = Token.receive(socket);
-                if (!rc.getIsConfirmation()) {
-                    System.out.printf("Token: seq=%d, #members=%d", rc.getSequence(), rc.length());
-                    for (Token.Endpoint endpoint : rc.getRing()) {
-                        System.out.printf(" (%s, %d)", endpoint.ip(), endpoint.port());
-                    }
-                    System.out.println();
-                    if (rc.length() == 1) {
-                        candidates.add(rc.poll());
-                        if (!first) {
-                            continue;
-                        }
-                    }
-                    first = false;
-                    for (Token.Endpoint candidate : candidates) {
-                        rc.append(candidate);
-                    }
-                    if (rc.length()>2){
-                        Token previous = new Token();
-                        previous.setIsConfirmation(true);
-                        previous.send(socket, rc.previous());
-                    }
-                    candidates.clear();
-                    Token.Endpoint next = rc.poll();
-                    rc.append(next);
-                    rc.incrementSequence();
-                    Thread.sleep(1000);
-                    rc.send(socket, next);
-                    socket.setSoTimeout(300);
-                    try {
-                        Token confirmation = Token.receive(socket);
-                        if (confirmation.getIsConfirmation()) {
-                            System.out.println("Token bestätigt");
-                        }
-                    }
-                    catch (SocketTimeoutException e) {
-                        System.out.println("Nächster Token wird gelöscht");
-                       rc.remove(next);
-                       next = rc.poll();
-                       rc.append(next);
-                       rc.send(socket, next);
-                    }
-
+                System.out.printf("Token: seq=%d, #members=%d", rc.getSequence(), rc.length());
+                for (Token.Endpoint endpoint : rc.getRing()) {
+                    System.out.printf(" (%s, %d)", endpoint.ip(), endpoint.port());
                 }
-
+                System.out.println();
+                if (rc.length() == 1) {
+                    candidates.add(rc.poll());
+                    if (!first) {
+                        continue;
+                    }
+                }
+                first = false;
+                for (Token.Endpoint candidate : candidates) {
+                    rc.append(candidate);
+                }
+                candidates.clear();
+                Token.Endpoint next = rc.poll();
+                rc.append(next);
+                rc.incrementSequence();
+                Thread.sleep(1000);
+                rc.send(socket, next);
             }
             catch (IOException e) {
                 System.out.println("Error receiving packet: " + e.getMessage());
             }
             catch (Exception e) {
-                System.out.println("Error: " + e.getMessage() + " " + Arrays.toString(e.getStackTrace()));
+                System.out.println("Error: " + e.getMessage());
             }
         }
     }
@@ -97,7 +71,7 @@ public class TokenRing {
         }
         catch (IOException e) {
             System.out.println("IO error: " + e.getMessage());
-            System.out.println(Arrays.toString(e.getStackTrace()));
+            System.out.println(e.getStackTrace());
         }
     }
 }
